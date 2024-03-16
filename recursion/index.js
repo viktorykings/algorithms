5f4ac6859481263a2c49ba5bcce5868c6a36adce
viktoria <kingsviktory@gmail.com>
2024-03-16 18:04:42 +0300

    feat: more recursion tasks

--- recursion/index.js
+++ recursion/index.js
@@ -4,3 +4,13 @@ function factorial(n){
     } else return n * factorial(n - 1)
 }
 console.log(factorial(5))
+
+function sum(arr){
+    if (arr.length === 0) return 0
+    if (arr.length === 1) return arr[0]
+    else{
+        let f = arr[arr.length - 1]
+        arr.pop()
+        return f + sum(arr)
+    }
+}
